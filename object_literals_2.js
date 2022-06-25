3ef7a581be503492782d6c82a6eae235df2c8ee6
pqx <l.r.slegt@gmail.com>
2022-06-25 18:36:42 +0200

    add object_literals

---
+++ object_literals_2.js
@@ -0,0 +1,11 @@
+let ln = 'last name';
+let person = {
+  'first name': 'Chandler',
+  [ln]: 'Bing',
+};
+console.log(person);
+
+let person2 = {
+  firstname: 'Chandler',
+};
+console.log(person2.firstname);
--- spread_operator_3.js
+++ spread_operator_3.js
@@ -1,3 +1,7 @@
+/* 
+Spread syntax (...) allows an iterable such as an array expression or string to be expanded in places where zero or more   arguments (for function calls) or elements (for array literals) are expected, or an object expression to be expanded in places where zero or more key-value pairs (for object literals) are expected 
+*/
+
 // rest is to combine individual components into an array
 // spread is to split an array into individual components
 
