ea96d8267d985a9683aa5197e1ea5602db9a45d4
pqx <l.r.slegt@gmail.com>
2022-06-25 19:55:23 +0200

    add for_of_loop_1

---
+++ for_of_loop_1.js
@@ -0,0 +1,16 @@
+let colors = ['Red', 'Blue', 'Green'];
+
+for (let index in colors) {
+  console.log(colors[index]);
+}
+
+// for of loop
+for (let color of colors) {
+  console.log(color);
+}
+
+let letters = 'ABC';
+
+for (let letter of letters) {
+  console.log(letter);
+}
